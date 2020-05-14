d9a2ca375a0006a0d544ede3d2a60eeb9291dbd6
Lia <lia>
2020-05-14 17:38:51 +0300

    added tail for 1 eaten fruit

--- main.py
+++ main.py
@@ -5,6 +5,23 @@ import random
 
 class Game:
 
+    def __init__(self, width, height):
+        self.field_width = width
+        self.field_height = height
+
+        self.snake = Snake(int(self.field_width / 2), int(self.field_height / 2))
+
+        self.fruit_x = random.randint(1, self.field_width - 2)
+        self.fruit_y = random.randint(1, self.field_height - 2)
+
+        self.score = 0
+
+        self.isGameOver = False
+
+        #self.fruitIsOnHead = False
+        
+        self.startListner()
+
     def onPress(self, key):
         try:
             k=key.char
@@ -12,38 +29,18 @@ class Game:
             k=key.name
         if k in ['w', 'a', 's', 'd']:
             if k == 'w':
-                self.direction = 'UP'
+                self.snake.direction = 'UP'
             if k == 'a':
-                self.direction = 'LEFT'
+                self.snake.direction = 'LEFT'
             if k == 's':
-                self.direction = 'DOWN'
+                self.snake.direction = 'DOWN'
             if k == 'd':
-                self.direction = 'RIGHT'
+                self.snake.direction = 'RIGHT'
 
     def startListner(self):
         self.listener = keyboard.Listener(self.onPress)
         self.listener.start() 
-
-    def __init__(self, width, height):
-        self.field_width = width
-        self.field_height = height
-
-        self.head_x = int(self.field_width / 2)
-        self.head_y = int(self.field_height / 2)
-
-        self.fruit_x = random.randint(1, self.field_width - 2)
-        self.fruit_y = random.randint(1, self.field_height - 2)
-
-        self.score = 0
-
-        self.direction = 'NONE'
-
-        self.isNotDed = True
-
-        self.fruitIsOnHead = False
-        
-        self.startListner()
-        
+    
     def clearScreen(self):
         os.system("clear")    
 
@@ -53,8 +50,11 @@ class Game:
             for x in range(0, self.field_width):
                 if y == 0 or y == self.field_height-1 or x == 0 or x == self.field_width-1:
                     print("#", end = "")
-                elif y == self.head_y and x == self.head_x:
+                elif y == self.snake.getHeadPos()[1] and x == self.snake.getHeadPos()[0]: # THIS MIGHT BE BAD SYNTAX BUT WE WILL SEE
                     print("O", end = "")
+                elif self.snake.isPartOfTail(x, y):
+                    print("o", end = "")
+                #TODO : check if tail element here 
                 elif y == self.fruit_y and x == self.fruit_x:
                     print("@", end = "")
                 else:
@@ -64,41 +64,112 @@ class Game:
 
     def randomFruit(self):
         self.fruit_x = random.randint(1, self.field_width - 2)
-        self.fruit_y = random.randint(1, self.field_height - 2)
-
-    def isFruitOnHead(self):
-        return self.fruit_x == self.head_x and self.fruit_y == self.head_y
-           
-    def isHeadInWall(self):
-        return self.head_x == 0 or self.head_x == self.field_width - 1 or self.head_y == 0 or self.head_y == self.field_height - 1
+        self.fruit_y = random.randint(1, self.field_height - 2)      
     
+    def isFruitOnHead(self):
+        return self.fruit_x == self.snake.getHeadPos()[0] and self.fruit_y == self.snake.getHeadPos()[1]
+
+    def isHeadInWall(self):
+        return  self.snake.getHeadPos()[0] == 0 or self.snake.getHeadPos()[0]  == self.field_width - 1 or self.snake.getHeadPos()[1] == 0 or self.snake.getHeadPos()[1] == self.field_height - 1
+
     def update(self):
-        if self.direction == 'UP':
-            self.head_y -= 1
-        if self.direction == 'DOWN':
-            self.head_y += 1
-        if self.direction == 'LEFT':
-            self.head_x -= 1
-        if self.direction == 'RIGHT':
-            self.head_x += 1
-        
-        if self.isHeadInWall():
-            self.isNotDed = False
+        self.snake.move()
 
         if self.isFruitOnHead():
-            self.fruitIsOnHead = True
+            self.snake.addTail()
+            self.score += 10
+            self.randomFruit()
+        
+        if self.isHeadInWall(): #TODO check if head in tail 
+            self.isGameOver = True
 
-    
+
     def playGame(self):
-        while self.isNotDed:
-            if self.fruitIsOnHead:
-                self.randomFruit()
-                self.score += 10
-                self.fruitIsOnHead = False
+        while not self.isGameOver:
             self.drawField()
-            time.sleep(0.2)
+            time.sleep(0.7)
             self.update()
         print("GAME OVER")
+
+class SnakeElement:
+    def __init__(self, element_x, element_y, isHead, number):
+        self.element_x = element_x
+        self.element_y = element_y
+
+        self.isHead = isHead
+
+        self.number = number
+    
+class Snake:
+    def __init__(self, startx, starty):
+        self.direction = 'NONE'
+
+        self.snake_elements_list = []
+        self.snake_elements_list.append(SnakeElement(startx, starty, True, 1))
+
+
+    def getHeadPos(self):
+        for snake_element in self.snake_elements_list:
+            if snake_element.isHead:
+                return [snake_element.element_x, snake_element.element_y]
+
+    def move(self):
+        if len( self.snake_elements_list ) == 1: # THIS IS HOW TO MOVE IF ONLY HEAD 
+            if self.direction == 'UP':
+                self.snake_elements_list[0].element_y -= 1
+            if self.direction == 'DOWN':
+                self.snake_elements_list[0].element_y += 1
+            if self.direction == 'LEFT':
+                self.snake_elements_list[0].element_x -= 1
+            if self.direction == 'RIGHT':
+                self.snake_elements_list[0].element_x += 1
+        else:
+            previous_head_index = -1
+            for i in range(0, len(self.snake_elements_list)):
+                if self.snake_elements_list[i].isHead == True:
+                    previous_head_index = i
+
+            for snake_element in self.snake_elements_list:
+                if snake_element.number == len(self.snake_elements_list):
+                    if self.direction == 'UP':
+                        snake_element.element_x = self.getHeadPos()[0]
+                        snake_element.element_y = self.getHeadPos()[1] - 1
+                    if self.direction == 'DOWN':
+                        snake_element.element_x = self.getHeadPos()[0]
+                        snake_element.element_y = self.getHeadPos()[1] + 1
+                    if self.direction == 'LEFT':
+                        snake_element.element_x = self.getHeadPos()[0] - 1
+                        snake_element.element_y = self.getHeadPos()[1]
+                    if self.direction == 'RIGHT':
+                        snake_element.element_x = self.getHeadPos()[0] + 1
+                        snake_element.element_y = self.getHeadPos()[1] 
+                    snake_element.isHead = True
+                    self.snake_elements_list[previous_head_index].isHead = False
+                    
+            for element in self.snake_elements_list:
+                if element.isHead :
+                    element.number = 1
+                else:
+                    element.number += 1
+
+
+    def addTail(self):
+        if len( self.snake_elements_list ) == 1: # THIS IS HOW TO MOVE IF ONLY HEAD 
+            if self.direction == 'UP':
+                self.snake_elements_list.append(SnakeElement(self.snake_elements_list[0].element_x, self.snake_elements_list[0].element_y + 1, False, 2))
+            if self.direction == 'DOWN':
+                self.snake_elements_list.append(SnakeElement(self.snake_elements_list[0].element_x, self.snake_elements_list[0].element_y - 1, False, 2))
+            if self.direction == 'LEFT':
+                self.snake_elements_list.append(SnakeElement(self.snake_elements_list[0].element_x + 1, self.snake_elements_list[0].element_y, False, 2))
+            if self.direction == 'RIGHT':
+                self.snake_elements_list.append(SnakeElement(self.snake_elements_list[0].element_x - 1, self.snake_elements_list[0].element_y, False, 2))
+
+    def isPartOfTail(self, x, y):
+        for snake_element in self.snake_elements_list:
+            if snake_element.element_x == x and snake_element.element_y == y and snake_element.isHead == False:
+                return True
+        return False
+
 
 def main():
     game = Game(51, 21)
